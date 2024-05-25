bcb15db45ed4c1e2e30f428e43b918b692bbb1f8
dbynarie <denirismanto2017@gmail.com>
2024-05-25 16:43:34 +0700

    update

---
+++ main.js
@@ -0,0 +1,17 @@
+const showModal = () => {
+    let modal = document.querySelector('.modalMenu')
+    modal.style.display = 'flex'
+
+}
+
+
+const hideModal = () => {
+
+    let modal = document.querySelector('.modalMenu')
+    modal.style.display = 'none'
+}
+
+const hideAlert = () => {
+    let aboutAlert = document.querySelector('.aboutAlert')
+    aboutAlert.style.display = 'none'
+}
